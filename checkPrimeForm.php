48216355f89d38786101aefa32690befea450261
William Salazar <was@williamsalazar.com>
2012-03-14 19:33:59 -0400

    This scipt asks the user in an HTML form setting for a number and the php script I created will return the prime number in that position.

---
+++ checkPrimeForm.php
@@ -0,0 +1,8 @@
+<form action = "checkPrimeNumber.php" method ="post">
+<fieldset>
+<legend>Prime Number</legend>
+What Nth prime number to you want to check for:<input type="text" size ="5" name = "prime"/><br />
+<input type="submit" value="Prime Number" name="submit"/>
+
+</fieldset>
+</form>
