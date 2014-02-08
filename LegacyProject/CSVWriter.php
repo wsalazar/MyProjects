c5358a7f8aa576ed97021cc4fcbcf71cfc8e72fa
Will Salazar <was@williamsalazar.com>
2014-02-08 01:05:49 -0500

    renamed file names.

---
+++ LegacyProject/CSVWriter.php
@@ -0,0 +1,139 @@
+<?php
+namespace \Vendor\Namespace\ClassName;
+
+/**
+  *@Entity @Table(name="CSVWriter")
+  **/
+
+class CSVWriter
+{
+
+
+	protected static $instance = null;
+
+	/**
+	  *@var int
+	  *@Id @Column(type="integer") @GeneratedValue 
+	  */	
+	protected $id;
+
+	/**
+	  *@var int
+	  *@Column(type="integer")
+	  */	
+	protected $HITS;
+
+	/**
+	  *@var int
+	  *@Column(type="integer")
+	  */	
+	protected $HITS1;
+
+	/**
+	  *@var int
+	  *@Column(type="integer")
+	  */	
+	protected $TotalHits;
+
+	/**
+	  *@var string
+	  *@Column(type="string")
+	  */	
+	protected $uri;
+
+	/**
+	  *@var string
+	  *@Column(type="string")
+	  */	
+	protected $Log1;
+
+	/**
+	  *@var string
+	  *@Column(type="string")
+	  */	
+	protected $Log2;
+
+	/**
+	  *@var string
+	  *@Column(type="string")
+	  */	
+	protected $Log3;
+
+	/**
+	  *@var string
+	  *@Column(type="string")
+	  */	
+	protected $Log4;
+
+	/**
+	  *@var string
+	  *@Column(type="string")
+	  */	
+	protected $Log5;
+
+	/**
+	  *@var string
+	  *@Column(type="string")
+	  */	
+	protected $Log6;
+
+	/*
+	 *@Description: This constructor is used so that a new instance can not be created from anywhere.
+	 */
+	private function __construct(){}
+
+	public static function getInstance()
+	{
+		if(!isset(static::$instance)){
+			static::$instance = new static;
+		}
+		return static::$instance;
+	}
+
+
+	public function getId()
+	{
+		return $this->id;
+	}
+
+
+	public function setHits1($hits)
+	{
+		$this->HITS = $hits;
+		return $this;
+		//return static::$instance;
+	}
+
+	public function setHits2($hits)
+	{
+		$this->HITS1 = $hits;
+		return $this;
+		//return static::$instance;
+	}
+
+	public function setTotalHits($hits)
+	{
+		$this->TotalHits = $hits;
+		return $this;
+		//return static::$instance;
+	}
+
+	public function setUri($uri)
+	{
+		$this->uri = $uri;
+		return $this;
+		//return static::$instance;
+	}	
+
+	public function setLogFiles($logFile1, $logFile2, $logFile3, $logFile4, $logFile5, $logFile6)
+	{
+		$this->Log1	= $logFile1;
+		$this->Log2	= $logFile2;
+		$this->Log3	= $logFile3;
+		$this->Log4	= $logFile4;
+		$this->Log5	= $logFile5;
+		$this->Log6	= $logFile6;
+		return $this;
+		//return static::$instance;
+	}
+}
